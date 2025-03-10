e4a7384307d5afd7f64f4809e09fa4a114f09bdb
sarahsbrna <sarahsabrina675@gmail.com>
2025-03-10 14:04:32 +0700

    mengubah dashboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -19,7 +19,7 @@
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
                         <li class="breadcrumb-item"><a href="/admin">Home</a></li>
-                        <li class="breadcrumb-item active">Dashboard</li>
+                        <li class="breadcrumb-item active">Dashboardddd</li>
                     </ol>
                 </div><!-- /.col -->
             </div><!-- /.row -->
