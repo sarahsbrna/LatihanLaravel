968a0e55b9a42db8dc62fc2798b80b679d96694a
sarahsbrna <sarahsabrina675@gmail.com>
2025-03-10 14:10:34 +0700

    ubah-feedback

--- resources/views/dashboard/feedback.blade.php
+++ resources/views/dashboard/feedback.blade.php
@@ -5,7 +5,7 @@
         <div class="row justify-content-center">
             <div class="col-md-12">
                 <div class="card">
-                    <div class="card-header">Form Feedback</div>
+                    <div class="card-header">Form Feedback123</div>
                     <div class="card-body">
                         <form method="POST" action="/dashboard-feedback">
                             @csrf
